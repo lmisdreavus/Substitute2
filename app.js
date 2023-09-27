1aaf6aeada8a16a73e32f7e273990ddc89d15544
lmisdreavus <67162145+lmisdreavus@users.noreply.github.com>
2023-09-27 15:30:13 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Google Charts 라이브러리 로드
+google.charts.load('current', {packages: ['corechart', 'bar']});
+
 let pokemons = [];
 
 // JSON 데이터 불러오기
@@ -23,16 +26,41 @@ document.getElementById('search').addEventListener('input', function () {
         </div>
         <div class="card-body">
           <p><strong>타입:</strong> ${pokemon.type}</p>
-          <p><strong>종족값:</strong> ${pokemon.baseStats}</p>
           <p><strong>특성:</strong> ${pokemon.abilities}</p>
           <p><strong>레벨 업으로 배우는 기술:</strong><br> ${pokemon.levelUpSkills.join('<br>')}</p>
           <p><strong>기술머신으로 배우는 기술:</strong><br> ${pokemon.machineSkills.join('<br>')}</p>
           <p><strong>알 부화로 배우는 기술:</strong><br> ${pokemon.eggSkills.join('<br>')}</p>
+          <div id="chart_div"></div> <!-- 차트를 표시할 div -->
         </div>
       </div>
     `;
     resultDiv.innerHTML = resultHtml;
+    drawChart(pokemon.baseStats); // 종족값을 바 차트로 그리기
   } else {
     resultDiv.innerHTML = '<p>검색 결과가 없습니다.</p>';
   }
 });
+
+// 종족값을 바 차트로 그리기
+function drawChart(baseStats) {
+  const data = google.visualization.arrayToDataTable([
+    ['능력치', '값', {role: 'style'}],
+    ['체력', baseStats[0], 'color: #FF0000'],
+    ['공격', baseStats[1], 'color: #FF7F00'],
+    ['방어', baseStats[2], 'color: #FFFF00'],
+    ['특수공격', baseStats[3], 'color: #00FF00'],
+    ['특수방어', baseStats[4], 'color: #0000FF'],
+    ['스피드', baseStats[5], 'color: #4B0082'],
+  ]);
+
+  const options = {
+    title: '종족값',
+    height: 400,
+    hAxis: {
+      minValue: 0,
+    },
+  };
+
+  const chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
