53bda34a59a53d903219848da3e495982782c58f
lmisdreavus <67162145+lmisdreavus@users.noreply.github.com>
2023-09-28 20:59:43 +0900

    Update app.js

--- app.js
+++ app.js
@@ -19,30 +19,45 @@ document.getElementById('search').addEventListener('input', function () {
 
   // 결과 출력
 if (pokemon) {
-    let resultHtml = `
-      <div class="card mb-3">
-        <div class="card-header">
-          <h2>${pokemon.name} (#${pokemon.number})</h2>
-        </div>
-        <div class="card-body">
-          <p><strong>타입:</strong> ${pokemon.type}</p>
-          <div><strong>종족값:</strong></div>
-          <div class="stat-container">
-            <div class="stat-bar" style="width: ${pokemon.baseStats.hp}%;">체력: ${pokemon.baseStats.hp}</div>
-            <div class="stat-bar" style="width: ${pokemon.baseStats.attack}%;">공격: ${pokemon.baseStats.attack}</div>
-            <div class="stat-bar" style="width: ${pokemon.baseStats.defense}%;">방어: ${pokemon.baseStats.defense}</div>
-            <div class="stat-bar" style="width: ${pokemon.baseStats.spAttack}%;">특수공격: ${pokemon.baseStats.spAttack}</div>
-            <div class="stat-bar" style="width: ${pokemon.baseStats.spDefense}%;">특수방어: ${pokemon.baseStats.spDefense}</div>
-            <div class="stat-bar" style="width: ${pokemon.baseStats.speed}%;">스피드: ${pokemon.baseStats.speed}</div>
-          </div>
-          <p><strong>특성:</strong> ${pokemon.abilities}</p>
-          <p><strong>레벨 업으로 배우는 기술:</strong><br> ${pokemon.levelUpSkills.join('<br>')}</p>
-          <p><strong>기술머신으로 배우는 기술:</strong><br> ${pokemon.machineSkills.join('<br>')}</p>
-          <p><strong>알 부화로 배우는 기술:</strong><br> ${pokemon.eggSkills.join('<br>')}</p>
-        </div>
+  let resultHtml = `
+    <div class="card mb-3">
+      <div class="card-header">
+        <h2>${pokemon.name} (#${pokemon.number})</h2>
       </div>
-    `;
-    resultDiv.innerHTML = resultHtml;
+      <div class="card-body">
+        <p><strong>타입:</strong> ${pokemon.type}</p>
+        
+        <table class="table table-striped mt-3">
+          <thead>
+            <tr>
+              <th>체력</th>
+              <th>공격</th>
+              <th>방어</th>
+              <th>특수공격</th>
+              <th>특수방어</th>
+              <th>스피드</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>${pokemon.baseStats[0]}</td>
+              <td>${pokemon.baseStats[1]}</td>
+              <td>${pokemon.baseStats[2]}</td>
+              <td>${pokemon.baseStats[3]}</td>
+              <td>${pokemon.baseStats[4]}</td>
+              <td>${pokemon.baseStats[5]}</td>
+            </tr>
+          </tbody>
+        </table>
+        
+        <p><strong>특성:</strong> ${pokemon.abilities}</p>
+        <p><strong>레벨 업으로 배우는 기술:</strong><br> ${pokemon.levelUpSkills.join('<br>')}</p>
+        <p><strong>기술머신으로 배우는 기술:</strong><br> ${pokemon.machineSkills.join('<br>')}</p>
+        <p><strong>알 부화로 배우는 기술:</strong><br> ${pokemon.eggSkills.join('<br>')}</p>
+      </div>
+    </div>
+  `;
+  resultDiv.innerHTML = resultHtml;
 }
 });
 
